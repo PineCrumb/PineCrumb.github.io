d94f24070d3811ba7d02d7f1e49321239125babe
PineCrumb <thomas9jg9@gmail.com>
2023-08-08 09:00:07 -0400

    Skill buttons + hovering

--- js/skills.js
+++ js/skills.js
@@ -8,6 +8,15 @@
 </div>
 
 
+Inside skills-frame-content grey-frame:
+
+<div class = "skill-button">
+    <div class = "skill-button-frame">
+        <img class = "skill-button-frame-icon" src = "assets/about-me/me-icon.png"/>
+        <div class = "skill-button-frame-title grey-frame-small">HTML</div>
+    </div>
+</div>   
+
 */
 
 function makeSkillsFrame(skillsData) {
@@ -21,6 +30,7 @@ function makeSkillsFrame(skillsData) {
     const skillsFrameContent = document.createElement('div');
     skillsFrameContent.classList.add('skills-frame-content');
     skillsFrameContent.classList.add('grey-frame');
+    skillsFrameContent.id = "skillButtonsContainer";
 
     skillsFrame.appendChild(skillsFrameHeader);
     skillsFrame.appendChild(skillsFrameContent);
@@ -30,5 +40,44 @@ function makeSkillsFrame(skillsData) {
 
 function makeSkillButton(skillData) {
     const skillButton = document.createElement('div');
+    skillButton.classList.add('skill-button');
+
+    const skillButtonFrame = document.createElement('div');
+    skillButtonFrame.classList.add('skill-button-frame');
+
+    const skillButtonFrameIcon = document.createElement('img');
+    skillButtonFrameIcon.classList.add('skill-button-frame-icon');
+    skillButtonFrameIcon.src = skillData.iconSrc;
+
+    const skillButtonFrameTitle = document.createElement('div');
+    skillButtonFrameTitle.classList.add('skill-button-frame-title');
+    skillButtonFrameTitle.classList.add('grey-frame-small');
+    skillButtonFrameTitle.textContent = skillData.title;
+
+    skillButtonFrame.appendChild(skillButtonFrameIcon);
+    skillButtonFrame.appendChild(skillButtonFrameTitle);
+    skillButton.appendChild(skillButtonFrame);
+
+
+    function MouseEnter() {
+        skillButtonFrame.style.top = '-10px';
+        skillButtonFrame.style.borderColor = 'rgba(255, 255, 255, 0.3)';
+        skillButtonFrameTitle.style.opacity = '1';
+        skillButton.style.cursor = 'pointer';
+    }
+
+    function MouseLeave() {
+        skillButtonFrame.style.top = '';
+        skillButtonFrame.style.borderColor = '';
+        skillButtonFrameTitle.style.opacity = '';
+        skillButton.style.cursor = '';
+    }
+
+    skillButton.addEventListener('mouseenter', MouseEnter);
+    skillButton.addEventListener('mouseleave', MouseLeave);
+
+    skillButton.addEventListener('touchstart', MouseEnter);
+    skillButton.addEventListener('touchend', MouseLeave);
+
     return skillButton;
 }
