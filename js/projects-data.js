980652392f74e0a6ce9b5ed605e4c31dc2a2b6cd
PineCrumb <thomas9jg9@gmail.com>
2023-08-13 18:22:38 -0400

    Filling out data

--- js/projects-data.js
+++ js/projects-data.js
@@ -1,19 +1,85 @@
 const projectsData = {
+    Tygoonery : {
+        title: "Pet Factory Simulator",
+        subTitle: "Creator and Developer",
+        date: "March 2023 - Present",
+        content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
+        thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
+        headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
+        headerText: ["Programming", "Designing", "Leadership"],
+        url: "robbery-simulator.html",
+    },
+    
     LandOfMerchants : {
-        title: "Land of Merchants (Supersocial)",
+        title: "Land of Merchants (by Supersocial)",
         subTitle: "Lead Developer",
         date: "April 2021 - Feb 2022",
         content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
         thumbnailSrc: "assets/LomThumbnail.png",
         headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
         headerText: ["Programming", "Designing", "Leadership"],
-        url: "test.html",
+        url: "robbery-simulator.html",
     },
     
+    Steelfront : {
+        title: 'SteelFront (War Game)',
+        subTitle: "Co-Creator and Developer",
+        date: "December 2022 - March 2023",
+        content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
+        thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
+        headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
+        headerText: ["Programming", "Designing", "Leadership"],
+        url: "robbery-simulator.html",
+    },
+
+    CityEightRoleplay : {
+        title: "City-8 Roleplay",
+        subTitle: "Gameplay Programmer (Commissioned)",
+        date: "Februrary 2021 - April 2021",
+        content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
+        thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
+        headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
+        headerText: ["Programming", "Designing", "Leadership"],
+        url: "robbery-simulator.html",
+    },
+
     RobberySimulator : {
-        title: "Robbery Simulator (Voldex)",
+        title: "Robbery Simulator (by Voldex)",
         subTitle: "Lead Programmer",
         date: "May 2020 - May 2021",
+        content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
+        thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
+        headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
+        headerText: ["Programming", "Designing", "Leadership"],
+        url: "robbery-simulator.html",
+    },
+
+    TitanGodSimulator : {
+        title: "Titan God Simulator (by Modblox)",
+        subTitle: "Fullstack Programmer",
+        date: "July 2019 - April 2020",
+        content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
+        thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
+        headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
+        headerText: ["Programming", "Designing", "Leadership"],
+        url: "robbery-simulator.html",
+    },
+
+    KingOfTheKrill : {
+        title: "King Of The Krill",
+        subTitle: "Creator and Developer",
+        date: "September 2019 - June 2020",
+        content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
+        thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
+        headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
+        headerText: ["Programming", "Designing", "Leadership"],
+        url: "robbery-simulator.html",
+    },
+
+    SkyOfRobloxia : {
+        title: "Sky of Robloxia 2.0",
+        subTitle: "Creator and Developer",
+        date: "2016 - 2018",
         content: "sdlf  sdd s s dfsdf fsds fdsdf sdf sdf sdf sdf sdf sdfsd fsdf  sfds dfsdf sdf sdf sdf  sdf sd fjsld j",
         thumbnailSrc: "assets/game-career/robbery-simulator/thumbnail.png",
         headerLogoSrc: "assets/game-career/robbery-simulator/logo.png",
