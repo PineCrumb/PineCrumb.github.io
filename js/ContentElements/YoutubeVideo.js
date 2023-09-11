fe248caa9b84d3a30b2bdfea5d7b35c93061697a
PineCrumb <thomas9jg9@gmail.com>
2023-09-10 23:05:02 -0400

    Still needs fixing

--- js/ContentElements/YoutubeVideo.js
+++ js/ContentElements/YoutubeVideo.js
@@ -4,8 +4,7 @@ export class YoutubeVideo extends ContentElementBase {
     makeContentElement(contentData) {
         const iframe = document.createElement('iframe');
         iframe.classList.add('project-content-media-content', 'grey-frame');
-        iframe.style.position = 'relative';
-        
+    
         iframe.title = 'YouTube Video Player';
         iframe.src = contentData.src;
         // iframe.frameBorder = '0';
